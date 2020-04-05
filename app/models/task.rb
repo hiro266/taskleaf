cda1d86fc1921c4d419e33a318d4110cb98316be
hiro266 <t1freejob@gmail.com>
2020-04-05 13:06:03 +0900

    task作成時の検証追加、失敗時の処理

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
+  validate :validate_name_not_including_comma_or_dot
   validates :name, presence: true, length: { maximum: 30 }
+
+  private
+
+  def validate_name_not_including_comma_or_dot
+    errors.add(:name, 'にカンマ、ドットを含めることはできません') if name&.include?(',') || name&.include?('.')
+  end
 end
