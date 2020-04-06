b46e25b188e499e1af97199dda97def70ebfb153
hiro266 <t1freejob@gmail.com>
2020-04-06 12:40:27 +0900

    他ユーザーが作成したタスクが表示されないSpec

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -20,6 +20,21 @@ describe 'タスク管理機能', type: :system do
         # 4.ログインするボタンをおす
         click_button 'ログインする'
       end
+    context 'ユーザーBがログインしている時' do
+      before do
+        # ユーザーBを作成しておく
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+        # ユーザーBでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+      it 'ユーザーAが作成したタスクが表示されない' do
+        # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+        expect(page).to have_no_content '最初のタスク'
+      end
+    end
 
       it 'ユーザーAが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
